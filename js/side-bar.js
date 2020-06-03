e3a29b0e544e48f05d4af2ed3f3b75b38e36af86
Pedrini <45887466+Pedriniii@users.noreply.github.com>
2020-06-03 17:46:23 -0300

    primeiro commit
    subindo tudo

---
+++ js/side-bar.js
@@ -0,0 +1,9 @@
+function sideButton_open() {
+    document.getElementById("mySideBar").style.width = "50%";
+    document.getElementById("mySideBar").style.display = "block";
+
+  }
+  
+  function w3_close() {
+    document.getElementById("mySideBar").style.display = "none";
+  }
