f73429aa08c36db0d4d8067026800a7702e4131d
Pedrini <45887466+Pedriniii@users.noreply.github.com>
2020-06-04 15:05:33 -0300

    adicionando evento de click na opção

--- js/side-bar.js
+++ js/side-bar.js
@@ -3,6 +3,14 @@ function sideButton_open() {
 
 }
 
-function w3_close() {
+function graphics() {
+  document.getElementById("graph").style.display = "flex";
+  document.getElementById("paymants").style.display = "none"
+  document.getElementById("mySideBar").style.display = "none";
+}
+  
+function inicio() {
+  document.getElementById("graph").style.display = "none";
+  document.getElementById("paymants").style.display = "flex"
   document.getElementById("mySideBar").style.display = "none";
 }
---
+++ js/sub-side.js
@@ -0,0 +1,64 @@
+document.getElementById("todos").style.color = "#0000FF"
+document.getElementById("todos").style.borderBottom = "solid #0000FF"
+//
+document.getElementById("graph1").style.color = "#0000FF"
+document.getElementById("graph1").style.borderBottom = "solid #0000FF"
+
+//
+
+function todosElement(){
+document.getElementById("todos").style.color = "#0000FF"
+document.getElementById("todos").style.borderBottom = "solid #0000FF"
+document.getElementById("pagos").style.color = "#000000"
+document.getElementById("pagos").style.borderBottom = "none"
+document.getElementById("recebidos").style.color = "#000000"
+document.getElementById("recebidos").style.borderBottom = "none"
+}
+
+function pagosElement(){
+document.getElementById("todos").style.color = "#000000"
+document.getElementById("todos").style.borderBottom = "none"
+document.getElementById("pagos").style.color = "#0000FF"
+document.getElementById("pagos").style.borderBottom = "solid #0000FF"
+document.getElementById("recebidos").style.color = "#000000"
+document.getElementById("recebidos").style.borderBottom = "none"
+}
+
+function recebidosElement(){
+document.getElementById("todos").style.color = "#000000"
+document.getElementById("todos").style.borderBottom = "none"
+document.getElementById("pagos").style.color = "#000000"
+document.getElementById("pagos").style.borderBottom = "none"
+document.getElementById("recebidos").style.color = "#0000FF"
+document.getElementById("recebidos").style.borderBottom = "solid #0000FF"
+}
+
+// FUNÇÕES PARA OS GRAFICOS (A FAZER AINDA)
+
+function graphElement1(){
+document.getElementById("graph1").style.color = "#0000FF"
+document.getElementById("graph1").style.borderBottom = "solid #0000FF"
+document.getElementById("graph2").style.color = "#000000"
+document.getElementById("graph2").style.borderBottom = "none"
+document.getElementById("graph3").style.color = "#000000"
+document.getElementById("graph3").style.borderBottom = "none"
+}
+
+function graphElement2(){
+document.getElementById("graph1").style.color = "#000000"
+document.getElementById("graph1").style.borderBottom = "none"
+document.getElementById("graph2").style.color = "#0000FF"
+document.getElementById("graph2").style.borderBottom = "solid #0000FF"
+document.getElementById("graph3").style.color = "#000000"
+document.getElementById("graph3").style.borderBottom = "none"
+}
+
+function graphElement3(){
+document.getElementById("graph1").style.color = "#000000"
+document.getElementById("graph1").style.borderBottom = "none"
+document.getElementById("graph2").style.color = "#000000"
+document.getElementById("graph2").style.borderBottom = "none"
+document.getElementById("graph3").style.color = "#0000FF"
+document.getElementById("graph3").style.borderBottom = "solid #0000FF"
+}
+                    
